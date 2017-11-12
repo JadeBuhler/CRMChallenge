854bf0843ca7c958d1ae0138485b3570bd2a7222
Jade <buhler.jb.jb@gmail.com>
2017-11-12 03:48:57 +0000

    Part 2: Added alphabetized and missing email actions to the customers controller and their associated views, also added styling to the customers.scss file

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,12 @@ class CustomersController < ApplicationController
     def index
         @customers = Customer.all
     end
+
+    def alphabetized
+        @customers = Customer.reorder('FullName')
+    end
+
+    def missing_email
+        @customers = Customer.where(EmailAddress: '')
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'customers#index'
 
-  get 'customers/index'
+  get '/', to: 'customers#index', as: 'home'
+  get '/customers/missing_email', to: 'customers#missing_email', as: 'missing_email'
+  get '/customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized'
 end
